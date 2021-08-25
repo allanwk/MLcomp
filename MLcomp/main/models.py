0295db5484fcad3cd92e33938bceb389e4e93656
allan <allankretzmann@hotmail.com>
2021-08-25 11:38:05 -0300

    Add botão download dataset, análise

--- MLcomp/main/models.py
+++ MLcomp/main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Dataset(models.Model):
     name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200, default="")
     short_desc = models.TextField(default="")
     desc = models.TextField(default="")
     card_image = models.ImageField(upload_to='images/', default=None)
