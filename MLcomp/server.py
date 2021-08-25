61546fd3f1bc1c8adc6d3c733f14edf2513f7601
allan <allankretzmann@hotmail.com>
2021-08-25 12:44:48 -0300

    Fix porta em server.py

--- MLcomp/server.py
+++ MLcomp/server.py
@@ -1,9 +1,11 @@
 from waitress import serve
+import os
     
 from MLcomp.wsgi import application
     
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
     try:
-        serve(application, port='8000')
+        serve(application, port=port)
     except Exception as e:
         print(e)
